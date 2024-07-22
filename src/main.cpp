ba5f031983f7fb98f4cabf3df7839aaf9b39d6eb
Gan Jun Kai <junkai@hey.com>
2024-07-22 22:44:26 +0800

    feat: setup SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,55 @@
-#include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    int result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+    if (result < 0) {
+        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return -1;
+    }
+
+    window = SDL_CreateWindow("Daedalus", 800, 600, SDL_WINDOW_VULKAN);
+    if (!window) {
+        SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
+        return -2;
+    }
+
+    renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer) {
+        SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
+        return -3;
+    }
+
+    SDL_Event event;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    SDL_Log("SDL Event Quit");
+                    quit = true;
+                    break;
+                case SDL_EVENT_KEY_DOWN:
+                    if (event.key.key == SDLK_ESCAPE) {
+                        SDL_Log("SDL ESC Key Quit");
+                        quit = true;
+                    }
+                    break;
+                default:;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 133, 193, 220, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
